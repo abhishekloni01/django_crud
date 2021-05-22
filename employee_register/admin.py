8e138f7ebc2229bcf25c9478e78351fdbb488832
Abhishek Loni <abhishekloni01@gmail.com>
2021-05-22 13:53:37 +0530

    edited models

--- employee_register/admin.py
+++ employee_register/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from employee_register.models import Position, Employee
+from employee_register.models import  Employee
 
 # Register your models here.
-admin.site.register(Position)
+# admin.site.register(Position)
 admin.site.register(Employee)
--- employee_register/models.py
+++ employee_register/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Position(models.Model):
-    title = models.CharField(max_length=100)
-    def __str__(self):
-        return self.title
+# class Position(models.Model):
+    
+#     title = models.CharField(max_length=100)
+#     def __str__(self):
+#         return self.title
     
 
 class Employee(models.Model):
+    EMP_CHOICES = (
+    ('Jnr Developer','Jnr Developer'),
+    
+    ('Accountant','Accountant'),
+    ('Sales Manager','Sales Manager'),
+    
+)
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=100)
     mobile = models.CharField(max_length=10)
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.CharField(choices= EMP_CHOICES, max_length=50)
